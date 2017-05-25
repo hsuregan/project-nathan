cb633f944364c002ecbe431f89ad72a2cf91a4d0
Regan Hsu <hsuregan@gmail.com>
2017-05-25 13:42:17 -0700

    first commit

---
+++ lab3a.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdbool.h>
+#include <stdint.h>
+#include <sys/param.h>
+
+
+int main(int argc, char **argv)
+{
+	if(argc != 2)
+	{
+		fprintf(stderr, "Incorrect number of arguments.");
+		exit(1);
+	}
+
+	
+
+}
